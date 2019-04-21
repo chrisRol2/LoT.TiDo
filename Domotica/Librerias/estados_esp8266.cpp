54c6538591b1c412cee93f2f4a8225ef5fbd5e6e
Chris Rol <christopher_rol@hotmail.es>
2019-04-21 18:43:30 -0300

    llama a funcion para enviar hora

--- Domotica/Librerias/estados_esp8266.cpp
+++ Domotica/Librerias/estados_esp8266.cpp
@@ -7,10 +7,16 @@
 //#include "displayLCD.h"
 #include "WiFi_esp8266.h"
 #include "Wire.h"
+#include "Reloj_NTP.h"
 DHT_Tiempo climaInte(DHTPIN, DHT_11);
+reloj_calendarioW reloj(-3);
+
 void clima_send(void){
 	climaInte.send();
 	return;
+}
+void reloj_send(void) {
+	reloj.send();
 }
 
 void temporalizador(unsigned long retraso,
@@ -23,6 +29,8 @@ void temporalizador(unsigned long retraso,
 
 void estados::permanente(){
 	static unsigned long temp_ret = 9000;
+	static unsigned long reloj_ret = 8500;
 	temporalizador(1000, &temp_ret, clima_send);
+	temporalizador(1000, &reloj_ret, reloj_send);
 	
 }
