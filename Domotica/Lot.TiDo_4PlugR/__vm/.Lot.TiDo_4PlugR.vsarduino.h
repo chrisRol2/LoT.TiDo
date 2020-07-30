41b450d15720fbef6900abf2564867aa32968fe6
chrisRol2 <Christopher_rol@hotmail.es>
2020-07-30 01:25:00 -0300

    update

--- Domotica/Lot.TiDo_4PlugR/__vm/.Lot.TiDo_4PlugR.vsarduino.h
+++ Domotica/Lot.TiDo_4PlugR/__vm/.Lot.TiDo_4PlugR.vsarduino.h
@@ -15,10 +15,8 @@
 #define _VSARDUINO_H_
 #define __ESP8266_esp8266__
 #define __ESP8266_ESP8266__
-#define _VMDEBUG 1
 #define __ets__
 #define ICACHE_FLASH
-#define NONOSDK22x_190703 1
 #define F_CPU 80000000L
 #define LWIP_OPEN_SRC
 #define TCP_MSS 536
