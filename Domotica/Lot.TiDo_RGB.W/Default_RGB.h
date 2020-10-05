55b7c4a2c2f5b66d6f33d50519ad1ba97571f05d
Chris Rol <christopher_rol@hotmail.es>
2020-10-05 03:44:12 -0300

    [05102020] -> creacion  constrantes de estados

--- Domotica/Lot.TiDo_RGB.W/Default_RGB.h
+++ Domotica/Lot.TiDo_RGB.W/Default_RGB.h
@@ -1,3 +1,12 @@
+/*
+*	File:	Default_RGB.h
+*	Author:	Christopher Roldan Sanchez
+*	Mail:	Christopher_Rol@hotmail.es
+*   Web:    https://www.lot-tido.com
+*	Created:
+*	Update Date:
+*	Description:
+*/
 #define RED_PIN   3
 #define GREEN_PIN 1
 #define BLUE_PIN  2
@@ -7,3 +16,9 @@
 #define password_d       "12345678"
 
 #define REFRESH_TIME 1
+
+#define STATIC 1
+#define FADE 2
+#define FLASH 3
+#define RANDOM 4
+#define PULSE 5
